55d760a84061aead40578c1613e8e58d4230eb9b
Anshul Atriek <atriekak@gmail.com>
2020-12-26 13:04:13 -0500

    52 ms

--- solutions/155. Min Stack.py
+++ solutions/155. Min Stack.py
@@ -4,24 +4,27 @@ class MinStack:
         """
         initialize your data structure here.
         """
-        self.mainStack = []
-        self.minStack = [float('inf')]
+        self.stack = []
+        self.min = float('inf')
 ​
     def push(self, x: int) -> None:
-        self.mainStack.append(x)
-        self.minStack.append(min(self.minStack[-1],x))
+        if x <= self.min:
+            self.stack.append(self.min)
+            self.min = x
+        self.stack.append(x)
         return
 ​
     def pop(self) -> None:
-        self.mainStack.pop()
-        self.minStack.pop()
+        popped = self.stack.pop()
+        if popped == self.min:
+            self.min = self.stack.pop()
         return
 ​
     def top(self) -> int:
-        return self.mainStack[-1]
+        return self.stack[-1]
 ​
     def getMin(self) -> int:
-        return self.minStack[-1]
+        return self.min
 ​
 ​
 # Your MinStack object will be instantiated and called as such:
