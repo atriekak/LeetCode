b87209a8c7e929f0352243b54f53be5be003f16f
Anshul Atriek <atriekak@gmail.com>
2020-12-29 23:52:12 -0500

    32 ms

--- solutions/409. Longest Palindrome.py
+++ solutions/409. Longest Palindrome.py
@@ -4,10 +4,14 @@ class Solution:
         #Space Complexity: O(1)
         #where, n is the length of string
         
-        hash_map = {}
+        hash_set = set()
+        count = 0
+        
         for char in s:
-            hash_map[char] = hash_map.get(char, 0) + 1
+            if char in hash_set:
+                hash_set.remove(char)
+                count += 2
+            else:
+                hash_set.add(char)
         
-        even_counts = [(c // 2) * 2 for c in hash_map.values()]
-        
-        return min(sum(even_counts) + 1, len(s))
+        return count + 1 if hash_set else count
