d877c1b7bcd7bf4ab540bbe1767cb0ae6d0ad532
Anshul Atriek <atriekak@gmail.com>
2020-12-30 00:04:40 -0500

    244 ms

--- solutions/560. Subarray Sum Equals K.py
+++ solutions/560. Subarray Sum Equals K.py
@@ -4,14 +4,18 @@ class Solution:
         #Space Complexity: O(n)
         #where, n is the length of nums
         
-        total = 0
+        running_sum = 0
         count = 0
         
+        #stores running_sum: total occurences of running_sum
         hash_map = {0: 1}
         
         for num in nums:
-            total += num
-            count += hash_map.get(total - k, 0)
-            hash_map[total] = hash_map.get(total, 0) + 1
+            running_sum += num
+            
+            complement = running_sum - k
+            count += hash_map.get(complement, 0)
+            
+            hash_map[running_sum] = hash_map.get(running_sum, 0) + 1
             
         return count
