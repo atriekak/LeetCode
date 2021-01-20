f5e21310b1b95a8803d3bc8f5e4211e4f4150147
Anshul Atriek <atriekak@gmail.com>
2021-01-20 00:58:53 -0500

    28 ms

--- solutions/199. Binary Tree Right Side View.py
+++ solutions/199. Binary Tree Right Side View.py
@@ -1,43 +1,18 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    #Solution 1
-    def rightSideView(self, root: TreeNode) -> List[int]:
-        #Approach: BFS
-        #Time Complexity: O(n)
-        #Space Complexity: O(n) // deque
+            return
         
-        result = []
-        
-        if not root:
-            return result
-        
-        de = deque()
-        de.append(root)
-        
-        while de:
-            sz = len(de)    #for level-order traversal
-            for i in range(sz):
-                popped = de.popleft()
-                temp = popped.val
-                
-                if popped.left:
-                    de.append(popped.left)
-                if popped.right:
-                    de.append(popped.right)
+        #logic
+        if level == len(self.result):
+            self.result.append(root.val)
             
-            result.append(temp)
-            
-        return result
+        self.dfs(root.right, level + 1)     #going to the right side first
+        #st.pop()
+        self.dfs(root.left, level + 1)
+    """
     
-    #Solution 2
+    #Solution 3
     """
     def rightSideView(self, root: TreeNode) -> List[int]:
-        #Approach: DFS, right-root-left
+        #Approach: DFS, left-root-right
         #Time Complexity: O(n)
         #Space Complexity: O(h) // under the hood
         
@@ -53,3 +28,14 @@ class Solution:
         #base
         if not root:
             return
+        
+        #logic
+        if level == len(self.result):
+            self.result.append(root.val)
+        else:   #since we are going to the left side first
+            self.result[level] = root.val
+            
+        self.dfs(root.left, level + 1)
+        #st.pop()
+        self.dfs(root.right, level + 1)
+    """
