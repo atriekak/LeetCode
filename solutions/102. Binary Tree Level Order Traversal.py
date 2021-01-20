3dda9538b5f0e0daa2cfe08064d72d62f2cd7618
Anshul Atriek <atriekak@gmail.com>
2021-01-20 00:59:05 -0500

    24 ms

--- solutions/102. Binary Tree Level Order Traversal.py
+++ solutions/102. Binary Tree Level Order Traversal.py
@@ -1,3 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+​
+from collections import deque
+​
+class Solution:
+    #Solution 1
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        #Approach: BFS
+        #Time Complexity: O(n)
+        #Space Complexity: O(n) // deque
+        
+        result = []
+        
+        if not root:
+            return result
+        
+        de = deque()
+        de.append(root)
+        
+        while de:
+            sz = len(de)    #for level-order traversal
+            temp = []
+            for i in range(sz):
+                popped = de.popleft()
+                temp.append(popped.val)
                 
                 if popped.left:
                     de.append(popped.left)
