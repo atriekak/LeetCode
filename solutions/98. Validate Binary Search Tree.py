4c0a65605844a2aebf3e1dde61281878263b90b1
Anshul Atriek <atriekak@gmail.com>
2021-01-21 16:44:30 -0500

    40 ms

--- solutions/98. Validate Binary Search Tree.py
+++ solutions/98. Validate Binary Search Tree.py
@@ -1,32 +1,3 @@
-        while root or st:
-            while root:
-                st.append(root)
-                root = root.left
-            root = st.pop()
-            if prev and prev.val >= root.val:
-                return False
-            prev = root
-            root = root.right
-            
-        return True
-    """
-    
-    #Solution 3
-    """
-    def isValidBST(self, root: TreeNode) -> bool:
-        #Approach: recursion; returns validity of BST at that node
-        #Time Complexity: O(n)
-        #Space Complexity: O(h)
-        #where, h is the height of the BST
-        
-        if not root:
-            return True
-        
-        self.prev = None
-        return self.inorder(root)
-    
-    def inorder(self, root):
-        #base
         if not root:
             return True
         
@@ -60,3 +31,21 @@
         self.inorder(root)
         return self.isValid
         
+    def inorder(self, root):
+        #base
+        if not root:
+            return
+        
+        #logic
+        self.inorder(root.left)
+        
+        if self.prev and self.prev.val >= root.val:
+            self.isValid = False
+            #will still get the correct answer if we comment the return out
+            #will just traverse through the entire tree each time
+            return 
+        
+        self.prev = root
+        
+        self.inorder(root.right)
+    """
