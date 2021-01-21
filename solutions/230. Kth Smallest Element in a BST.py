11d768a8da78a47f506cdd06b7ea7bd826c66d56
Anshul Atriek <atriekak@gmail.com>
2021-01-21 15:51:32 -0500

    44 ms

--- solutions/230. Kth Smallest Element in a BST.py
+++ solutions/230. Kth Smallest Element in a BST.py
@@ -1,55 +1,63 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    #Solution 1
-    def kthSmallest(self, root: TreeNode, k: int) -> int:
-        #Approach: Recursive
-        #Time Complexity: O(n)
-        #Space Complexity: O(h) #under the hood
+        #Space Complexity: O(1)
 ​
-        self.k = k
-        self.result = None
-        self.kthSmallestHelper(root, k)
+        curr = root
         
-        return self.result.val
-    
-    def kthSmallestHelper(self, root, k):
-        #base
-        if not root:
-            return
-        
-        #logic
-        self.kthSmallestHelper(root.left, k)
-        self.k -= 1
-        
-        if self.k == 0:
-            self.result = root
-            return
-        
-        self.kthSmallestHelper(root.right, k)
+        while curr:
+            if not curr.left:
+                k -= 1
+                if k == 0:
+                    return curr.val
+                
+                curr = curr.right
+            
+            else:
+                predecessor = curr.left
+                while predecessor.right and predecessor.right != curr:
+                    predecessor = predecessor.right
+                
+                #left not visited before; move left and establish link
+                if not predecessor.right:
+                    predecessor.right = curr
+                    curr = curr.left
+                
+                #left visited before; move right and remove link
+                else: #predecessor.right == curr
+                    predecessor.right = None
+                
+                    k -= 1
+                    if k == 0:
+                        return curr.val
+                
+                    curr = curr.right
     
     #Solution 2
     """
     def kthSmallest(self, root: TreeNode, k: int) -> int:
-        #Approach: Iterative
+        #Approach: Recursive DFS
         #Time Complexity: O(n)
-        #Space Complexity: O(h)
+        #Space Complexity: O(h) #under the hood
+​
+        self.k = k
+        self.result = None
+        self.kthSmallestHelper(root, k)
         
-        st = []
-        count = 0
-        while root or st:
-            while root:
-                st.append(root)
-                root = root.left
-            root = st.pop()
-            
-            count += 1
-            if count == k:
-                return root.val
-            
-            root = root.right
+        return self.result.val
+    
+    def kthSmallestHelper(self, root, k):
+        #base
+        if not root:
+            return
+        
+        #logic
+        self.kthSmallestHelper(root.left, k)
+        self.k -= 1
+        
+        if self.k == 0:
+            self.result = root
+            return
+        
+        if self.result == None: #to suppress recursion after the solution is found
+            self.kthSmallestHelper(root.right, k)
     """
+    
+    #Solution 3
