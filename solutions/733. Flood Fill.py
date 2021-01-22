11347ac4571cf4b93b81ebf60495ff01b97e6ced
Anshul Atriek <atriekak@gmail.com>
2021-01-22 01:09:54 -0500

    68 ms

--- solutions/733. Flood Fill.py
+++ solutions/733. Flood Fill.py
@@ -34,3 +34,42 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         #Approach: BFS
         #Time Complexity: O(m * n)
+        #Space Complexity: O(m * n)
+        
+        if image[sr][sc] == newColor:
+            return image
+        
+        m = len(image)
+        n = len(image[0])
+        
+        color = image[sr][sc]
+        dirArr = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+        
+        de = deque()
+        de.append((sr, sc))
+        image[sr][sc] = newColor
+        
+        while de:
+            popped = de.popleft()
+            print(popped)
+            
+            for d in dirArr:
+                r = popped[0] + d[0]
+                c = popped[1] + d[1]
+                
+                if r >= 0 and r < m and c >= 0 and c < n and image[r][c] == color:
+                    de.append((r, c))
+                    image[r][c] = newColor
+                    
+        return image
+    """
+    
+    #Solution 3
+    """
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        #Approach: DFS, iterative
+        #Time Complexity: O(m * n)
+        #Space Complexity: O(m * n)
+        
+        if image[sr][sc] == newColor:
+            return image
