b8945336731defdf917d4f8ad9e6f90f08a5b6f1
Anshul Atriek <atriekak@gmail.com>
2021-01-31 00:23:06 -0500

    60 ms

---
+++ solutions/341. Flatten Nested List Iterator.py
@@ -0,0 +1,59 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+​
+class NestedIterator:
+    #Approach: Stack of iterators
+    #Time Complexity: O(l / n)
+    #Space Complexity: O(d)
+    #where l is number of lists and n is the number of integers in the nestedList
+    #and, d is the maximum nesting depth
+    
+    def __init__(self, nestedList: [NestedInteger]):
+        self.st = []
+        self.st.append(iter(nestedList))
+    
+    def next(self) -> int:
+        return self.nextEl.getInteger()
+    
+    def hasNext(self) -> bool:
+        while self.st:
+            try:
+                self.nextEl = next(self.st[-1])
+            except:
+                self.st.pop()
+                continue
+​
+            if self.nextEl.isInteger():
+                return True
+            else:
+                self.st.append(iter(self.nextEl.getList()))
+        return False
+​
+"""
+from collections import deque
+class NestedIterator:
+    #Approach: Flattening using recursion
+    #Time Complexity: O(1)      // flattening is a one time operation
+    #Space Complexity: O(n)
+    #where, n is the total number of integers in the nestedList
+    
+    def __init__(self, nestedList: [NestedInteger]):
