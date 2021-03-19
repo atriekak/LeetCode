4e2c7884a72723a46cea164abd93cf069ab9bd4f
Anshul Atriek <atriekak@gmail.com>
2021-03-18 23:01:46 -0400

    56 ms

--- solutions/120. Triangle.py
+++ solutions/120. Triangle.py
@@ -23,11 +23,11 @@ class Solution:
         for i in range(1, len(triangle)):
             for j in range(len(triangle[i])):
                 if j == 0:
-                    triangle[i][j] = triangle[i][j] + triangle[i-1][j]
+                    triangle[i][j] += triangle[i-1][j]
                 elif j == i:
-                    triangle[i][j] = triangle[i][j] + triangle[i-1][j-1]
+                    triangle[i][j] += triangle[i-1][j-1]
                 else:
-                    triangle[i][j] = triangle[i][j] + min(triangle[i-1][j-1], triangle[i-1][j])
+                    triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j])
         
         return min(triangle[-1])
     """
