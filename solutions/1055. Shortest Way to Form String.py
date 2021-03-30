e202503b1840d4d61124a80fcee0a23da32bbcec
Anshul Atriek <atriekak@gmail.com>
2021-03-29 21:20:44 -0400

    32 ms

--- solutions/1055. Shortest Way to Form String.py
+++ solutions/1055. Shortest Way to Form String.py
@@ -26,7 +26,7 @@ class Solution:
                 result += 1
                 idx = 0
                 
-            #'using' char at index charsDict[idx][char] in the source string
+            #'using up' char at index charsDict[idx][char] in the source string
             idx = charsDict[idx][char] + 1
             
         return result
@@ -34,27 +34,3 @@ class Solution:
     #Solution 2
     """
     def shortestWay(self, source: str, target: str) -> int:
-        #Approach: Greedy w/ two pointers
-        #Time Complexity: O(m * n)
-        #Space Complexity: O(1)
-        #where, m and n are the lengths of strings 'source' and 'target' respectively
-        
-        s = 0
-        t = tPrev = 0
-        
-        result = 0
-        while t < len(target):
-            while s < len(source) and t < len(target):
-                if target[t] == source[s]:
-                    t += 1
-                s += 1
-            
-            if t == tPrev:
-                return -1
-            
-            tPrev = t
-            s = 0
-            result += 1
-            
-        return result
-    """
