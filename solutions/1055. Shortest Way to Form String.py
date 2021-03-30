e15ba2a20a31197e5251b202deac25f1e1db8996
Anshul Atriek <atriekak@gmail.com>
2021-03-29 21:19:57 -0400

    32 ms

--- solutions/1055. Shortest Way to Form String.py
+++ solutions/1055. Shortest Way to Form String.py
@@ -1,25 +1,60 @@
 class Solution:
+    #Solution 1
     def shortestWay(self, source: str, target: str) -> int:
-        #Approach: Greedy w/ two pointers
-        #Time Complexity: O(m * n)
-        #Space Complexity: O(1)
+        #Approach: Greedy w/ HashMaps
+        #Time Complexity: O(m + n)
+        #Space Complexity: O(m)
         #where, m and n are the lengths of strings 'source' and 'target' respectively
         
-        s = 0
-        t = tPrev = 0
-        
-        result = 0
-        while t < len(target):
-            while s < len(source) and t < len(target):
-                if target[t] == source[s]:
-                    t += 1
-                s += 1
+        #stores if and when a character appears next
+        charsDict = [None for _ in source]
+        for i in reversed(range(len(source))):
+            charsDict[i] = {} if i == len(source) - 1 else charsDict[i + 1].copy()
+            char = source[i]
+            charsDict[i][char] = i
             
-            if t == tPrev:
+        result = 1
+        idx = 0     # in source
+        
+        for char in target:
+            #char is in the target string but not the source string
+            if char not in charsDict[0]:
                 return -1
             
-            tPrev = t
-            s = 0
-            result += 1
+            #char in the source string, but not to the right of idx
+            if idx == len(source) or char not in charsDict[idx]:
+                result += 1
+                idx = 0
+                
+            #'using' char at index charsDict[idx][char] in the source string
+            idx = charsDict[idx][char] + 1
             
         return result
+    
+    #Solution 2
+    """
+    def shortestWay(self, source: str, target: str) -> int:
+        #Approach: Greedy w/ two pointers
+        #Time Complexity: O(m * n)
+        #Space Complexity: O(1)
+        #where, m and n are the lengths of strings 'source' and 'target' respectively
+        
+        s = 0
+        t = tPrev = 0
+        
+        result = 0
+        while t < len(target):
+            while s < len(source) and t < len(target):
+                if target[t] == source[s]:
+                    t += 1
+                s += 1
+            
+            if t == tPrev:
+                return -1
+            
+            tPrev = t
+            s = 0
+            result += 1
+            
+        return result
+    """
