6e2a70712054c6c7d44b2d7e7f40b4b25d982b40
Anshul Atriek <atriekak@gmail.com>
2021-03-30 23:57:00 -0400

    24 ms

---
+++ solutions/1056. Confusing Number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def confusingNumber(self, N: int) -> bool:
+        #Time Complexity: O(n)
+        #Space Complexity: O(n)
+        #where, n is the length of the digit N
+        
+        oldN, newN = N, 0
+        rotMap = {0: 0, 1: 1, 6: 9, 8: 8, 9: 6}
+        
+        while N:
+            rem = N % 10
+            if rem not in rotMap:
+                return False
+            newN = newN * 10 + rotMap[rem]
+            
+            N = N // 10
+            
+        return newN != oldN
