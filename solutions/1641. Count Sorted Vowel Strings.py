068ecbb213f45e93a4059f4c3a45a95fd666577e
Anshul Atriek <atriekak@gmail.com>
2021-04-11 20:49:52 -0400

    20 ms

--- solutions/1641. Count Sorted Vowel Strings.py
+++ solutions/1641. Count Sorted Vowel Strings.py
@@ -6,14 +6,11 @@ class Solution:
         #Space Complexity: O(n)
         
         vowels = 5
-        dp = [[1 for _ in range(n)] for _ in range(vowels)]
+        dp = [[1 for _ in range(n+1)] for _ in range(vowels)]
         
         for i in range(1, vowels):
-            for j in range(n):
-                if j == 0:
-                    dp[i][j] = i + 1
-                else:
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+            for j in range(1, n+1):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         
         return dp[-1][-1]
     
