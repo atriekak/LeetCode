371deef8fc4a6a11969049a07af1bc3a4c07d694
Anshul Atriek <atriekak@gmail.com>
2021-04-13 23:02:44 -0400

    828 ms

--- solutions/84. Largest Rectangle in Histogram.py
+++ solutions/84. Largest Rectangle in Histogram.py
@@ -6,19 +6,19 @@ class Solution:
         #Space Complexity: O(n)
         #where, n is the length of heights
         
-        heights.append(0)
-        st = []
+        heights.append(0)   # to pop everything at the end
+        st = [-1]           # to calculate the width when nothing else in st
     
         i = 0
         maxAr = 0
 ​
         while i < len(heights):
-            if not st or heights[st[-1]] <= heights[i]:
+            if st[-1] == -1 or heights[st[-1]] <= heights[i]:
                 st.append(i)
                 i += 1
             else:
                 popped = st.pop()
-                ar = heights[popped] * ((i - st[-1] - 1) if st else i)
+                ar = heights[popped] * (i - st[-1] - 1)
                 maxAr = max(maxAr, ar)
             
         return maxAr
@@ -47,25 +47,3 @@ class Solution:
                 idx = i
                 
         self.maxAr = max(self.maxAr, heights[idx] * (high - low + 1))
-        self.helper(heights, low, idx - 1)
-        self.helper(heights, idx + 1, high)
-    """
-    
-    #Solution 3
-    """
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        #Approach: Brute Force
-        #Time Complexity: O(n^2)
-        #Space Complexity: O(1)
-        #where, n is the length of heights
-        
-        n = len(heights)
-        maxAr = 0
-        for i in range(n):
-            minHt = inf
-            for j in range(i, n):
-                minHt = min(minHt, heights[j])
-                maxAr = max(maxAr, minHt * (j - i + 1))
-                
-        return maxAr
-    """
