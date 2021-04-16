448eb561726b9fae28173df1951738e4faf44dac
Anshul Atriek <atriekak@gmail.com>
2021-04-15 20:44:32 -0400

    20 ms

--- solutions/1056. Confusing Number.py
+++ solutions/1056. Confusing Number.py
@@ -2,7 +2,7 @@ class Solution:
     def confusingNumber(self, N: int) -> bool:
         #Time Complexity: O(n)
         #Space Complexity: O(1)
-        #where, n is the length of the digit N
+        #where, n is the length of the number N
         
         oldN, newN = N, 0
         rotMap = {0: 0, 1: 1, 6: 9, 8: 8, 9: 6}
